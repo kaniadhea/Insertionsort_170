8698b62cf09b80279fe40c1ad8a76666c1fd00b1
DELL <DELL@DESKTOP-KLJI07S>
2023-03-24 16:41:35 +0700

    memanggil variable

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,4 +7,12 @@ int n;									// Membuat Variable inputan n
 void input() {					//Procedure Input 
 	while (true)
 	{
-		cout << "Memasukkan Jumlah Data pada Array : "; //Membuat Inputan n 
+		cout << "Memasukkan Jumlah Data pada Array : "; //Membuat Inputan Jumlah elem Array 
+		cin >> n;										//Memanggil Variable inputan n
+
+		if (n <= 20) {									//Membuat kondisi n tidak lebih dari 20 
+			break;
+		}
+		else
+		{
+			cout << "n\Array yang anda masukkan maksimal 20 elemen. \n"; //Menampilkan pesan jika data lebih dari 20
